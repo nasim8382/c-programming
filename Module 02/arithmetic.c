104c1febbb6e9b2f343637fc20e19790945768be
Nasim Reza <nasim8382@gmail.com>
2023-04-05 19:30:58 +0600

    module 01 name changed

--- Module 02/arithmetic.c
+++ Module 02/arithmetic.c
@@ -2,5 +2,14 @@
 
 int main()
 {
+    int x = 0;
+        if (x == 1) {
+            if (x >= 0) {
+                printf("true ");
+            }
+            else {
+                printf("false");
+            }
+        }
     return 0;
 }
