221bd9c190343b91fa3fb06ae2d134e01222472c
Nasim Reza <nasim8382@gmail.com>
2023-04-05 15:48:02 +0600

    if else condition

---
+++ Module 02/conditional_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int money;
+    scanf("%d", &money);
+
+    if (money >= 100)
+    {
+        printf("I will eat burger...");
+    }
+    else{
+        printf("I will not eat burger...");
+    }
+    return 0;
+}
