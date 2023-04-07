74acce4c177aba4d783024ad25fd41064a233133
Nasim Reza <nasim8382@gmail.com>
2023-04-07 16:13:08 +0600

    module 03 practice done

---
+++ Module 03/do-while_loop.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    // int i = 1;
+    // do
+    // {
+    //     printf("%d\n", i);
+    //     i++;
+    // }
+    // while(i<=7);
+    // return 0;
+
+    int i = 0;
+    while (i = 0) {
+        printf("Hello\n");
+    }
+    return 0;
+}
---
+++ Module 03/for_loop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    for (i=1; i<=16; i=i*2)
+    {
+        printf("%d\n", i);
+        // printf("Hello\n");
+    }
+    return 0;
+}
